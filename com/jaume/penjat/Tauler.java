fd5eab11633f4ce764c6bac79cd6da37e8b77e5f
Antonio Morell <amorellb@cifpfbmoll.eu>
2021-01-30 11:35:10 +0100

    Created Getters, Setters and Constructors
    Se ha creado un constructor vacío, un constructor para inicializar la variable "intents" y los getters y setters de la variable

--- com/jaume/penjat/Tauler.java
+++ com/jaume/penjat/Tauler.java
@@ -2,4 +2,27 @@ package com.jaume.penjat;
 
 public class Tauler {
     private Integer intents;
+
+    /*
+     * Constructores
+     * */
+    // Constructor vacío
+    public Tauler() {
+    }
+    // Constructor
+    public Tauler(Integer intents) {
+        this.intents = intents;
+    }
+
+    /*
+    * Getters y Setters
+    * */
+    // Getter
+    public Integer getIntents() {
+        return intents;
+    }
+    // Setter
+    public void setIntents(Integer intents) {
+        this.intents = intents;
+    }
 }
