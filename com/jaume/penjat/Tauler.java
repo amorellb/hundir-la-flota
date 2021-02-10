7058bd7329c9bbdac1f78bf39f9f213a151912bb
Antonio Morell <amorellb@cifpfbmoll.eu>
2021-02-10 23:55:54 +0100

    Borrar código basura y poner todo bonito

--- com/jaume/penjat/Tauler.java
+++ com/jaume/penjat/Tauler.java
@@ -105,25 +105,10 @@ public class Tauler {
 
     // Método hasGuanyat
     public Boolean hasGuanyat() {
-        /*String palabra = "";
-        String palabradeloscojones = "";
-        for (Character letra : paraulaSecreta) {
-            palabradeloscojones = palabra.concat(letra.toString());
-        }
-        if (String.join("", palabraEndevinada).equals(palabradeloscojones)) {
-            System.out.println(palabra);
-            System.out.println(palabradeloscojones);
-            return true;
-        }
-        System.out.println("palabra " + palabra);
-        System.out.println("palabradeloscojones " + palabradeloscojones);
-        return false;*/
-
         if (String.valueOf(paraulaSecreta).equals(String.join("", palabraEndevinada))) {
             return true;
         }
         return false;
     }
-
 }
 
